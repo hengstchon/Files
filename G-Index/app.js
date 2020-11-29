69a8f42fd318485089cf42f9c1fa129faeb3f18a
hengstchon <hengstchon@gmail.com>
2020-11-29 22:09:19 +0100

    add support for potplayer

--- G-Index/app.js
+++ G-Index/app.js
@@ -805,6 +805,9 @@ const file_video = path => {
     <a class="mdui-btn mdui-ripple mdui-color-theme-accent" href="mpv://${url}">
       <i class="mdui-icon material-icons">play_circle_outline</i> mpv
     </a>
+    <a class="mdui-btn mdui-ripple mdui-color-theme-accent" href="potplayer://${url}">
+      <i class="mdui-icon material-icons">play_circle_outline</i> Potplayer
+    </a>
     <a class="mdui-btn mdui-ripple mdui-color-theme-accent" href="intent:${url}#Intent;package=com.mxtech.videoplayer.pro;S.title=${path};end">
       <i class="mdui-icon material-icons">play_circle_outline</i> mx player
     </a>
